6c4e804d7a7bacfc31eb306da0fc79361b5fdbe1
William Crownover <williamcrownover1@gmail.com>
2021-08-10 23:57:56 -0700

    Developed Blogpost.js model

---
+++ models/Blogpost.js
@@ -0,0 +1,49 @@
+// Required modules
+const { Model, DataTypes } = require('sequelize');
+// Required files
+const sequelize = require('../config/connection');
+
+// Extend model
+class Blogpost extends Model {}
+
+// Initialize model
+Blogpost.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    dateCreated: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    modelName: 'blogpost',
+  }
+);
+
+// Export
+module.exports = Blogpost;
