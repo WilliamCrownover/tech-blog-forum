11e9a238e62da469b85bd8e5b0a23c2982f12ba9
William Crownover <williamcrownover1@gmail.com>
2021-08-10 23:53:14 -0700

    Developed User.js model

---
+++ models/User.js
@@ -0,0 +1,54 @@
+// Required modules
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+// Required files
+const sequelize = require('../config/connection');
+
+// Extend model
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+// Initialize model
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    modelName: 'user',
+  }
+);
+
+// Export
+module.exports = User;
