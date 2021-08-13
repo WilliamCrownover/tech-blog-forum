77a71c78a0cd0da48eb894dda8222199a194e866
William Crownover <williamcrownover1@gmail.com>
2021-08-13 01:11:40 -0700

    Dashboard route and page now only shows the logged in user's blogposts

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -2,12 +2,26 @@
 // Required modules
 const router = require('express').Router();
 // Required files
+const { Blogpost, User } = require('../models');
 const withAuth = require('../utils/auth');
+const serialize = require('../utils/serialize');
 
 // Home route
 router.get('/', withAuth, async (req, res) => {
     try {
+        const blogpostData = await Blogpost.findAll({
+            include: [
+                { model: User }
+            ],
+            where: {
+                userId: req.session.user_id
+            }
+        })
+
+        const blogposts = blogpostData.map(serialize);
+
         res.render('dashboard', {
+            blogposts,
             logged_in: req.session.logged_in
         });
         
