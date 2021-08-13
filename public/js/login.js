728216e3e49c4a883793e062b2ae9e7b0157c88c
William Crownover <williamcrownover1@gmail.com>
2021-08-13 00:25:28 -0700

    Expanded login.js to process a login attempt

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,25 @@
+// Submit login form to validate account exists and login
+const loginFormSubmit = async (event) => {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Incorrect login');
+        }
+    }
+};
+
 // Submit signup form entry to create user account
 const signupFormSubmit = async (event) => {
     event.preventDefault();
@@ -19,6 +41,11 @@ const signupFormSubmit = async (event) => {
         }
     }
 };
+
+// Listen for the login form submission
+document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormSubmit);
 
 // Listen for the signup form submission
 document
